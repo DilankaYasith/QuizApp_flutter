2f01a0dea204acb8cb31df60dfb2ba31d78fe15a
Dilanka Yasith <123799744+DilankaYasith@users.noreply.github.com>
2023-07-20 17:16:38 +0530

    updated questions screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,6 +12,8 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(context) {
-    return Container();
+    return const Center(
+      child: Text("Question screen"),
+    );
   }
 }
